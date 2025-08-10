ec6c049e0b498dc9d49fb97d8c326d5ad91d5c4f
labli <lovelyjoypreyes@gmail.com>
2025-08-10 21:53:21 +0800

    remove the job_info in routes and replace with job.py

--- src/core_api/routes/job_crud.py
+++ src/core_api/routes/job_crud.py
@@ -2,13 +2,13 @@
 from fastapi import APIRouter
 from uuid import UUID
 from typing import Any
-from ..models.job_info import jobmodel
+from ..models.job import Job
 
 router = APIRouter(prefix="/jobs")
 
 #Create
 @router.post("/", summary="Create a new job")
-async def create_job(job: jobmodel) -> dict[str, Any]:
+async def create_job(job: Job) -> dict[str, Any]:
     # TODO: Insert into MongoDB
     return {"message": "Job created", "job": job}
 
@@ -20,7 +20,7 @@ async def get_job(job_id: UUID) -> dict[str, Any]:
 
 # UPDATE
 @router.put("/{job_id}", summary="Update a job by ID")
-async def update_job(job_id: UUID, job: jobmodel) -> dict[str, Any]:
+async def update_job(job_id: UUID, job: Job) -> dict[str, Any]:
     # TODO: Update job in MongoDB
     return {"message": "Job updated", "job_id": str(job_id), "job": job}
 
