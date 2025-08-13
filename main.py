77ba3d092a550ed6cb882ca63ae6c042e8f78f1a
xRomory <claneralarn@gmail.com>
2025-08-13 13:38:52 +0800

    Remove Job Crud Routes on main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-from src.core_api.routes import job_crud
 from src.core_api.routes import job_route
 
 app = FastAPI()
-app.include_router(job_crud.router)
 app.include_router(job_route.router)
