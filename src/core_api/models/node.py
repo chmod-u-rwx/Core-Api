2ee18970afbf2beea1eefe29a929aa6a2784a438
labli <lovelyjoypreyes@gmail.com>
2025-08-14 21:29:37 +0800

    added the node model with testing

---
+++ src/core_api/models/node.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel, Field
+
+class Node(BaseModel):
+    node_id: str = Field(..., min_length=1, description="Id for node")
+    job_slots: int = Field(..., ge=0, description="Job slots on the node")
