3c8136775fddd57254e238e4ee57caa07347416c
xRomory <clyncemiro@gmail.com>
2025-09-09 03:25:48 +0800

    Connected Backend to Frontend

--- src/core_api/app.py
+++ src/core_api/app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .config import CORS_ORIGINS
 from src.core_api.routes import master_node_route, job_route, node_routes, users_route
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(job_route.router)
 app.include_router(master_node_route.router)
 app.include_router(node_routes.router)
--- src/core_api/config.py
+++ src/core_api/config.py
@@ -9,6 +9,8 @@ load_dotenv()
 # ex:
 API_KEY = os.getenv("API_KEY") 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+CORS_ORIGIN = os.getenv("CORS_ORIGINS", "")
+CORS_ORIGINS = [origin.strip() for origin in CORS_ORIGIN.split(",") if origin.strip()]
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 DATABASE_URL = os.getenv("MONGODB_URI", "mongodb://localhost:27017/")
 DATABASE_NAME = os.getenv("MONGODB_DATABASE", "test_db")
