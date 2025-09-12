6ce2f5b72bb84cab675c2e93a4b04060ed6d4011
xRomory <clyncemiro@gmail.com>
2025-09-12 10:27:35 +0800

    Request Metrics Model done

--- src/core_api/app.py
+++ src/core_api/app.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .config import CORS_ORIGINS
+from .config import ALLOW_ORIGINS
 from src.core_api.routes import master_node_route, job_route, node_routes, users_route
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=ALLOW_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
---
+++ src/core_api/models/requests.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timezone
+from typing import Optional
+from uuid import UUID
+from pydantic import BaseModel, Field
+from src.core_api.models.payloads import JobRequestPayload, JobResponsePayload
+
+class Requests(BaseModel):
+    request_id: UUID = Field(...)
+    job_id: UUID = Field(...)
+    worker_id: UUID = Field(...)
+    vm_id: UUID = Field(...)
+    request_payload: JobRequestPayload = Field(...)
+    response_payload: JobResponsePayload = Field(...)
+    is_success: Optional[bool] = Field(default=None)
+    error_detail: Optional[str] = Field(default=None)
+    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    execution_time: Optional[float] = Field(default=None, description="Execution time in seconds")
+    transaction_id: Optional[UUID] = Field(default=None, description="Reference to Job expense")
