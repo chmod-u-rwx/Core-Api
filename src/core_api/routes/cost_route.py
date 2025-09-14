86b06bc93012503b9fe0eda3e3501dd794c23277
xRomory <clyncemiro@gmail.com>
2025-09-14 22:24:56 +0800

    Cleaned summary under cost_route

--- src/core_api/routes/cost_route.py
+++ src/core_api/routes/cost_route.py
@@ -5,7 +5,7 @@ router = APIRouter(prefix="/cost", tags=["Resource Cost"])
 CPU_CORE_COST_PER_SECOND = 0.03
 RAM_GB_COST_PER_SECOND = 0.05
 
-@router.get("/", summary="Get current cost per ")
+@router.get("/", summary="Get resource cost per seconds")
 def get_resource_cost():
     return {
         "cpu_core_cost_per_second": CPU_CORE_COST_PER_SECOND,
